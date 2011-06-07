6ac532476d7b8ad02a15662ddd0e792accb75a5a
Alex Crichton <alex@alexcrichton.com>
2011-06-07 01:04:51 -0700

    Set a host to send from for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,4 +54,6 @@ Oddit::Application.configure do
   config.active_support.deprecation = :notify
 
   config.force_ssl = true
+
+  config.action_mailer.default_url_options = { :host => 'oddit.me' }
 end
