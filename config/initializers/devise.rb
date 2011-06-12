e5061f26115db5ed07c24091684e89fe951863d2
Alex Crichton <alex@alexcrichton.com>
2011-06-12 10:06:29 -0700

    Fixing api tokens for facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -186,10 +186,10 @@ Devise.setup do |config|
 
   require 'openid/store/filesystem'
 
-  api_key    = ENV['API_KEY']    || '2299a39eedae4dac0cf2edb0741bcd25'
-  app_secret = ENV['APP_SECRET'] || 'a0b890b1d884000ad6505a7b7390c611'
+  fb_app_id = ENV['FB_APP_ID'] || '133075676730741'
+  fb_secret = ENV['FB_SECRET'] || 'a0b890b1d884000ad6505a7b7390c611'
 
-  config.omniauth :facebook, api_key, app_secret, :scope => ''
+  config.omniauth :facebook, fb_app_id, fb_secret, :scope => 'email'
   config.omniauth :open_id,
     OpenID::Store::Filesystem.new(Rails.root.join('tmp').to_s)
 
