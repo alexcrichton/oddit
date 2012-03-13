4b712ec03961de318153342d26823a6b1812ed58
Alex Crichton <alex@alexcrichton.com>
2012-03-12 21:27:19 -0500

    Be sure to precompile everything

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Oddit::Application.configure do
   # config.serve_static_assets = true
 
   # When precompiling, precompile all js/css
-  # config.assets.precompile.push(/\w+\.(?:js|css).*/)
+  config.assets.precompile.push(/\w+\.(?:js|css).*/)
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
